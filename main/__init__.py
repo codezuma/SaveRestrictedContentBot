657d40f45a81657dca7a66c89b792cdc54dbaf6e
chandresh patidar <patidarchandresh2002@gmail.com>
2023-03-08 19:46:54 -0800

    add pokemon

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = '17809227'
+API_HASH = '24c6288eee3e68db783afaf3df669534'
+BOT_TOKEN = '5105784060:AAEuQbnUpzwHslcL_cyoArS5F1Q2IVtN_Qk'
+AUTH = '1414938408'
+FORCESUB = 'safalam_clssses'
+SESSION = 'BQAv3AV1FipqJcAAZ0VMwrOH-8MaBt07cSOswapa24CdPa1TcYJxRpZw1PB_K89vqpGJHMI4jl5p95-qQ7trrbpblI0scwlnZehK5h1Wfb9yKVIeh3TJaEM39nGLKEUAEh3rIaRYwOvBQNbDe-bq5-XHnakL1pF2TnL-GSv9h4sjnA-45G1OZnuoO2256k4cr45tO96K1SZr1PAeW5tqCP9w-tBGGNvBODxyh1kfJB9xRSjaGFyzq7rR9dvrOgveO6OP6TMMAdJf-aimf1SoOLt3U7LonX-jCRfhv_qYN2FW4032QVcns-qoCW3uOtZdUJRy8Ze56kPXtHXKXMXBWsJXVFY_KAA'
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
